2379bed50a346eca97460f50441fa8a56e1db37e
Pavel Pravosud <rwz@duckroll.ru>
2011-05-24 14:30:32 +0700

    optimized uuid a little

--- lib/uniquify-uuid/uuid.rb
+++ lib/uniquify-uuid/uuid.rb
@@ -1,22 +1,18 @@
-module Uniquify; end
-
-if RUBY_VERSION > '1.9'
-  require 'securerandom'
-  
-  module Uniquify::UUID
+module Uniquify
+  module UUID
     extend self
-    def generate
-      SecureRandom.uuid
+    
+    if RUBY_VERSION > '1.9'
+      require 'securerandom'
+      def generate
+        SecureRandom.uuid
+      end
+    else
+      require 'simple_uuid'
+      def generate
+        SimpleUUID::UUID.new.to_guid
+      end
     end
-  end
-
-else
-  require 'simple_uuid'
-  
-  module Uniquify::UUID
-    extend self
-    def generate
-      SimpleUUID::UUID.new.to_guid
-    end
+    
   end
 end
